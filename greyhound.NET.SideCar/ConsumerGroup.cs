48863ff86dfb31eb28247b6ac9c980b03a091a83
Bar Amsalem <98264234+Bar-Amsalem-Wix@users.noreply.github.com>
2022-06-19 09:24:44 +0300

    WIP

---
+++ greyhound.NET.SideCar/ConsumerGroup.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace greyhound.NET.SideCar
+{
+    public sealed class ConsumerGroup
+    {
+
+        public Dictionary<string, ConsumerTopic> Topics { get; } = new Dictionary<string, ConsumerTopic>();
+
+    }
+
+
+}
+
---
+++ greyhound.NET.SideCar/ConsumerTopic.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Com.Wixpress.Dst.Greyhound.Sidecar.Api;
+
+namespace greyhound.NET.SideCar
+{
+    public sealed class ConsumerTopic
+    {
+        public EventHandler<Domain.Record> Handler { get; }
+
+        public ConsumerTopic(EventHandler<Domain.Record> handler)
+        {
+            Handler = handler ?? ((s,e) => { });
+        }
+    }
+
+
+}
+
